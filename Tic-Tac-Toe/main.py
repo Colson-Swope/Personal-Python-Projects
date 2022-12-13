f276c56c0b2c2154bb4c1d5137e0792ec2a8e906
Colson-Swope <colsonjswope@gmail.com>
2022-12-13 15:57:25 -0800

    Added start menu

---
+++ Tic-Tac-Toe/main.py
@@ -0,0 +1,13 @@
+import turtle 
+
+#Displays welcome message 
+print("Welcome to Tic Tac Toe")
+
+#Collects the name of both players 
+playerOne = input("Player 1, please enter your name: ")
+playerTwo = input("Player 2, please enter your name: ")
+
+#Displays the welcome message with both player names 
+print("Welcome,", playerOne, "and", playerTwo)
+
+menuOption = input("Ready to begin? Enter 'S' to start, or 'H' for help: ")
