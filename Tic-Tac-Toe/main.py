58a2ca834e91320a0384bdc08bc1aa0ba8c64d03
Colson-Swope <colsonjswope@gmail.com>
2022-12-13 16:29:56 -0800

    Update main.py
    Player names / main menu framework is now complete. Help documentation needs to be added, as well as the main game loop.

--- Tic-Tac-Toe/main.py
+++ Tic-Tac-Toe/main.py
@@ -1,5 +1,11 @@
 import turtle 
 
+def mainGame():
+    print("Insert game here")
+
+def helpDocumentation():
+    print("Insert game documentation here")
+
 #Displays welcome message 
 print("Welcome to Tic Tac Toe")
 
@@ -10,4 +16,12 @@ playerTwo = input("Player 2, please enter your name: ")
 #Displays the welcome message with both player names 
 print("Welcome,", playerOne, "and", playerTwo)
 
-menuOption = input("Ready to begin? Enter 'S' to start, or 'H' for help: ")
+while True:
+    menuOption = input("Ready to begin? Enter 'S' to start, or 'H' for help: ")
+
+    if 'S' == menuOption:
+        mainGame()
+        break
+
+    if 'H' == menuOption:
+        helpDocumentation()
