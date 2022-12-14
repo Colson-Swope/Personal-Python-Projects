f8251d02a032dab78e3e6af245f91b46eb6af715
Colson-Swope <colsonjswope@gmail.com>
2022-12-13 16:32:42 -0800

    Added comments to Tic-Tac-Toe code

--- Tic-Tac-Toe/main.py
+++ Tic-Tac-Toe/main.py
@@ -1,8 +1,10 @@
 import turtle 
 
+#Contains code for the main game loop
 def mainGame():
     print("Insert game here")
 
+#Contains help documentation text 
 def helpDocumentation():
     print("Insert game documentation here")
 
@@ -16,12 +18,16 @@ playerTwo = input("Player 2, please enter your name: ")
 #Displays the welcome message with both player names 
 print("Welcome,", playerOne, "and", playerTwo)
 
+#Contains code for the start menu 
 while True:
+    #Asks player to either enter S or H 
     menuOption = input("Ready to begin? Enter 'S' to start, or 'H' for help: ")
 
+    #If the player enters "S", start the game 
     if 'S' == menuOption:
         mainGame()
         break
 
+    #If the player enters "H", display the help documentation and start the loop again 
     if 'H' == menuOption:
         helpDocumentation()
