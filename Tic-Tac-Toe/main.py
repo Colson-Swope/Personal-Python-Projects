c819a529365e610c5084483a37b51cf63d1c6741
Colson-Swope <colsonjswope@gmail.com>
2022-12-13 17:26:58 -0800

    Added move tracker

--- Tic-Tac-Toe/main.py
+++ Tic-Tac-Toe/main.py
@@ -39,6 +39,9 @@ def mainGame():
 def helpDocumentation():
     print("Insert game documentation here")
 
+def A1X():
+    print("Place holder")
+
 #Displays welcome message 
 print("Welcome to Tic Tac Toe")
 
@@ -62,5 +65,60 @@ while True:
     #If the player enters "H", display the help documentation and start the loop again 
     if 'H' == menuOption:
         helpDocumentation()
+
+while True: 
+    print("Your turn,", playerOne)
+
+    playeroneMove = input("What is your move?: ")
+
+    print("Your turn,", playerTwo)
+
+    playertwoMove = input("What is your move?: ")
+    
+
+    if "A1" == playeroneMove:
+        print("You moved to A1")
+    if "A2" == playeroneMove:
+        print("You moved to A2")
+    if "A3" == playeroneMove:
+        print("You moved to A3")
+
+    if "B1" == playeroneMove:
+        print("You moved to B1")
+    if "B2" == playeroneMove:
+        print("You moved to B2")
+    if "B3" == playeroneMove:
+        print("You moved to B3")
+
+    if "C1" == playeroneMove:
+        print("You moved to C1")
+    if "C2" == playeroneMove:
+        print("You moved to C2")
+    if "C3" == playeroneMove:
+        print("You moved to C3")
+
+
+
+    if "A1" == playertwoMove:
+        print("You moved to A1")
+    if "A2" == playertwoMove:
+        print("You moved to A2")
+    if "A3" == playertwoMove:
+        print("You moved to A3")
+
+    if "B1" == playertwoMove:
+        print("You moved to B1")
+    if "B2" == playertwoMove:
+        print("You moved to B2")
+    if "B3" == playertwoMove:
+        print("You moved to B3")
+
+    if "C1" == playertwoMove:
+        print("You moved to C1")
+    if "C2" == playertwoMove:
+        print("You moved to C2")
+    if "C3" == playertwoMove:
+        print("You moved to C3")
+
 
 
